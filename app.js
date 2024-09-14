1000555f2c35efbaa65a19d5f7faf21f5de7e5d0
Aftab1311 <md.aftabehsan@gmail.com>
2024-09-14 08:04:05 +0530

    Revert "Update app.js"
    This reverts commit 19aa8a5f955753f4b2e15a4cf5b332b4b1191a53.

--- app.js
+++ app.js
@@ -14,12 +14,7 @@ dotenv.config();
 const app = express();
 
 // middlewares
-const corsOptions = {
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-};
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use("/user", userRouter);
 app.use("/admin", adminRouter);
